f8894bbdfff73d970bafedb0028145865e7775d1
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-11 13:51:14 +0100

    added frame and tests for function: bitboard::util::pop_msb

--- engine/src/bitboard/utils.rs
+++ engine/src/bitboard/utils.rs
@@ -3,6 +3,9 @@ pub fn pop_lsb(value: &mut u64) -> usize {
   return 0;
 }
 
+pub fn pop_msb(value: &mut u64) -> usize {
+  return 0;
+}
 
 
 // <----- TESTS ----->
@@ -32,4 +35,23 @@ mod tests {
     }
   }
 
+  #[test]
+  fn pop_msb_test() {
+    // test setup
+    let test_values: [u64; 6] = [
+      0x86D6_8EB0_96A8_8D1C,
+      0x0000_0000_0000_0001,
+      0x3809_24AF_A7AE_8129,
+      0x0277_DA36_3B31_86D9,
+      0x0000_C1C3_201C_0DB1,
+      0x0000_0203_0DE4_E944
+    ];
+    let expected_values: [usize; 6] = [63, 0, 61, 57, 47, 41];
+
+    // tests
+    for index in 0..6 {
+      let mut test_value = test_values[index];
+      assert_eq!(pop_lsb(&mut test_value), expected_values[index])
+    }
+  }
 }
