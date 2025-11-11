d0e6ce81ce3a2534d468ece0d892870934b43cb9
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-11 14:06:43 +0100

    fixed bad test values in test utils::tests::pop_lsb_test

--- engine/src/bitboard/utils.rs
+++ engine/src/bitboard/utils.rs
@@ -23,14 +23,14 @@ mod tests {
 
     // test setup
     let test_values: [u64; 6] = [
-      0x0000_0000_0000_0000,
+      0x8000_0000_0000_0000,
       0x4E91_CF05_713E_451B,
       0xD588_2D58_6962_34B0,
       0x9581_3335_DCAB_1DD4,
       0xBEAC_DBE0_903A_AC00,
       0x01E8_C895_A6F0_0000
     ];
-    let expected_values: [usize; 6] = [64, 0, 4, 2, 10, 20];
+    let expected_values: [usize; 6] = [63, 0, 4, 2, 10, 20];
 
     // tests
     for index in 0..6 {
