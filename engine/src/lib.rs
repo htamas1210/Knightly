30448b5f3df5622959b7d41598da78a1976a62da
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-15 20:37:06 +0100

    switched return type of is_game_over from bool to GameEnd

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -6,13 +6,14 @@ pub mod movetype;
 pub mod gameend;
 
 use chessmove::ChessMove;
+use gameend::GameEnd;
 
 pub fn get_available_moves(fen: &str) -> Vec<ChessMove> {
   println!("get_available_moves answered");
   return vec![];
 }
 
-pub fn is_game_over(fen: &str) -> bool {
+pub fn is_game_over(fen: &str) -> Option<GameEnd> {
   println!("is_game_over answered");
-  return false;
+  return None;
 }
