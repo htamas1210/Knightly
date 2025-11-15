c8b6bc35d52a0835cd9c555dcd25eba3f35312ba
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-15 20:08:54 +0100

    added library functions: get_available_moves and is_game_over

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -3,3 +3,15 @@ pub mod chessmove;
 pub mod piecetype;
 pub mod boardsquare;
 pub mod movetype;
+
+use chessmove::ChessMove;
+
+pub fn get_available_moves(fen: &str) -> Vec<ChessMove> {
+  println!("get_available_moves answered");
+  return vec![];
+}
+
+pub fn is_game_over(fen: &str) -> bool {
+  println!("is_game_over answered");
+  return false;
+}
