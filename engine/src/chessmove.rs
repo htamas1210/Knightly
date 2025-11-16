a8970053f9bb3f0ccb1874fcf202cefc31dbcf98
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-16 19:05:52 +0100

    added constructor for castling moves in chessmove.rs

--- engine/src/chessmove.rs
+++ engine/src/chessmove.rs
@@ -50,4 +50,22 @@ impl ChessMove {
       promotion_piece: promotion_piece
     }
   }
+
+  pub fn castle(
+    piece_type: PieceType,
+    from_square: BoardSquare,
+    to_square: BoardSquare,
+    rook_from: BoardSquare,
+    rook_to: BoardSquare
+  ) -> Self {
+    return Self {
+      move_type: MoveType::Quiet,
+      piece_type: piece_type,
+      from_square: from_square,
+      to_square: to_square,
+      rook_from: rook_from,
+      rook_to: rook_to,
+      promotion_piece: None
+    }
+  }
 }
