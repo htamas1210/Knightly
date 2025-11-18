d8da8085804b175cdfbe7bdfe3b6f454ef193bb9
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-18 17:19:31 +0100

    added enum BitMoveType to bitboard::bitmove.rs for readability

--- engine/src/bitboard/bitmove.rs
+++ engine/src/bitboard/bitmove.rs
@@ -1,7 +1,7 @@
 
 pub struct BitMove {
 
-  pub move_type: u8,
+  pub move_type: BitMoveType,
   pub from_square: u8,
   pub to_square: u8,
   pub promotion_piece: Option<u8>
@@ -12,10 +12,17 @@ impl BitMove {
   #[inline]
   pub fn quiet(from: u8, to: u8, promotion_piece: Option<u8>) -> Self {
     return Self {
-      move_type: 0,
+      move_type: BitMoveType::Quiet,
       from_square: from,
       to_square: to,
       promotion_piece: promotion_piece
     };
   }
+}
+
+pub enum BitMoveType {
+  Quiet,
+  Capture,
+  Castle,
+  En_Passant
 }
