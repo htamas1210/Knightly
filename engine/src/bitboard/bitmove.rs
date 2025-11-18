c420d8b3dd54bf98b64076abc9d201da40d7cef4
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-18 17:17:11 +0100

    added constructor for quiet moves in bitboard::bitmove.rs

--- engine/src/bitboard/bitmove.rs
+++ engine/src/bitboard/bitmove.rs
@@ -6,3 +6,16 @@ pub struct BitMove {
   pub to_square: u8,
   pub promotion_piece: Option<u8>
 }
+
+impl BitMove {
+
+  #[inline]
+  pub fn quiet(from: u8, to: u8, promotion_piece: Option<u8>) -> Self {
+    return Self {
+      move_type: 0,
+      from_square: from,
+      to_square: to,
+      promotion_piece: promotion_piece
+    };
+  }
+}
