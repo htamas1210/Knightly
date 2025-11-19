48272d6d70ebbb44c5e6dad8ea4536f7fec011a1
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-19 20:35:46 +0100

    added skeleton for main move generation method to bitboard::movegen

---
+++ engine/src/bitboard/movegen.rs
@@ -0,0 +1,19 @@
+use super::board::Board;
+use super::movebuffer::MoveBuffer;
+
+impl Board {
+
+  pub fn collect_moves(&mut self, buffer: &mut MoveBuffer, temp_buffer: &mut MoveBuffer) -> bool {
+    buffer.clear();
+    self.calc_pinned_squares();
+    let check_info = self.check_test();
+
+    match check_info.check_count {
+      // 0 => self.collect_all_moves(),
+      // 1 => self.collect_moves_single_check(),
+      // 2 => self.collect_king_evasion(),
+      _ => panic!("More than 2 checking pieces found as the same time!")
+    }
+    return check_info.check_count > 0;
+  }
+}
