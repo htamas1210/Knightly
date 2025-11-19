85a7fa37ef47b141e0d13b56e9c096a8332e4e03
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-19 15:22:15 +0100

    added parameterless constructor for struct MoveBuffer

--- engine/src/bitboard/movebuffer.rs
+++ engine/src/bitboard/movebuffer.rs
@@ -5,3 +5,13 @@ pub struct MoveBuffer {
   buffer: [BitMove; 256],
   count: usize
 }
+
+impl MoveBuffer {
+
+  pub fn new() -> Self {
+    return Self {
+      buffer: [BitMove::quiet(0, 0, None); 256],
+      count: 0
+    };
+  }
+}
