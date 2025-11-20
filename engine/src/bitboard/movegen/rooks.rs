0dd9a3d0473aedb83e093b9be9e5f89961d9a9e2
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-20 12:23:14 +0100

    implemented move generation method for rook capture moves

--- engine/src/bitboard/movegen/rooks.rs
+++ engine/src/bitboard/movegen/rooks.rs
@@ -33,4 +33,23 @@ impl Board {
       }
     }
   }
+  fn add_rook_captures(&self, buffer: &mut MoveBuffer, move_mask: u64) {
+    let offset = 6 * self.side_to_move as usize;
+    let mut rooks: u64 = self.bitboards[3 + offset];
+    let opponents = self.occupancy[1 - self.side_to_move as usize];
+    while rooks != 0 {
+      let next_sq: u32 = pop_lsb(&mut rooks);
+      let mut attacks: u64 = self.get_pseudo_rook_moves(next_sq) & opponents & move_mask;
+      attacks = self.get_pin_masked_moves(attacks, next_sq);
+
+      while attacks != 0 {
+        let to_sq = pop_lsb(&mut attacks);
+        buffer.add(BitMove::capture(
+          next_sq as u8,
+          to_sq as u8,
+          None
+        ));
+      }
+    }
+  }
 }
