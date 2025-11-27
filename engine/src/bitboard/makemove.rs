2394da84ce295778ad3a5321d613a9ba34ee370d
Varga Dávid Lajos <vargadavidlajos@mailbox.unideb.hu>
2025-11-27 23:26:59 +0200

    added skeleton for method bitboard::makemove::make_move

--- engine/src/bitboard/makemove.rs
+++ engine/src/bitboard/makemove.rs
@@ -1,6 +1,36 @@
 
+
+use super::bitmove::BitMoveType;
+use super::bitmove::BitMove;
 use super::board::Board;
 
 impl Board {
 
+  #[inline]
+  pub fn make_move(&mut self, played_move: &BitMove) {
+    let move_type = played_move.move_type();
+
+    match move_type {
+      BitMoveType::Quiet => {
+        
+      }
+      BitMoveType::Capture => {
+        
+      }
+      BitMoveType::Castle => {
+        
+      }
+      BitMoveType::EnPassant => {
+        
+      }
+    }
+
+    self.occupancy[2] = self.occupancy[0] | self.occupancy[1];
+
+    if self.en_passant_square != 0 {
+      self.en_passant_square = 0u64;
+    }
+
+    self.side_to_move = 1 - self.side_to_move;
+  }
 }
